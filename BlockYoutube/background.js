7e440246dd0ca491fc79eef684dbe06cef35777f
colour-of-art <149279801+colour-of-art@users.noreply.github.com>
2024-01-23 21:26:33 +1100

    Update background.js

--- BlockYoutube/background.js
+++ BlockYoutube/background.js
@@ -1,9 +1,40 @@
+// background.js
+
+let isBlocking = false;
+
 chrome.webRequest.onBeforeRequest.addListener(
     function(details) {
-      if (details.url.includes("youtube.com")) {
-        return {cancel: true};
-      }
+        if (isBlocking && details.url.includes("youtube.com")) {
+            return {cancel: true};
+        }
     },
     {urls: ["<all_urls>"]},
     ["blocking"]
-  );
+);
+
+chrome.runtime.onInstalled.addListener(function() {
+    chrome.storage.sync.set({isBlocking: false}, function() {
+        console.log('Extension initialized with blocking set to false');
+    });
+});
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        if (request.toggleBlocking !== undefined) {
+            isBlocking = request.toggleBlocking;
+            chrome.storage.sync.set({isBlocking: isBlocking}, function() {
+                console.log('Blocking state set to ' + isBlocking);
+            });
+        }
+    }
+);
+
+// Add a browser action
+chrome.browserAction.onClicked.addListener(function(tab) {
+    isBlocking = !isBlocking;
+    chrome.storage.sync.set({isBlocking: isBlocking}, function() {
+        console.log('Blocking state toggled to ' + isBlocking);
+        // Send a message to the content script (if needed) to update UI
+        chrome.tabs.sendMessage(tab.id, {toggleBlocking: isBlocking});
+    });
+});
