d006bfd14ce02ffd9dc4f296d5317dd886d1f79f
colour-of-art <149279801+colour-of-art@users.noreply.github.com>
2024-03-01 07:17:25 +1100

    Delete BlockYoutube directory

--- BlockYoutube/content.js
+++
@@ -1,9 +0,0 @@
-chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-        if (request.toggleBlocking !== undefined) {
-            // Update the UI or perform other actions in the content script based on the blocking state
-            console.log('Blocking state in content script: ' + request.toggleBlocking);
-            // You can add your UI update logic here
-        }
-    }
-);
